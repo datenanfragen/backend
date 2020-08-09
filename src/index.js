6389f0185c56e176ff425e4666e021530c085ba8
Benjamin Altpeter <benjamin@oldpeter.de>
2020-08-09 22:16:34 +0200

    Listen on 0.0.0.0 (necessary for Uberspace)

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ const knex = require('knex')(require('../knexfile').development);
 const init = async () => {
     const server = Hapi.server({
         port: 3000,
-        host: 'localhost',
+        host: '0.0.0.0',
         routes: {
             cors: true,
             files: {
