49c70ef1f98b6e14bff72f2f9cbcec31c86a6cdb
Malte <45886910+mal-tee@users.noreply.github.com>
2020-08-09 12:37:46 +0200

    Ported suggest-api (#1)
    * Ported suggest-api
    
    * moved `for` check and removed debug output

---
+++ src/handlers/suggest/handler.js
@@ -0,0 +1,60 @@
+const config = require('../../../config.json');
+const { Octokit } = require('@octokit/rest');
+const octokit = new Octokit({
+    auth: config.suggest.github_token,
+    userAgent: 'Datenanfragen.de suggest-api',
+});
+
+function messageBody(data) {
+    return `The following suggestion was submitted through the website:
+
+\`\`\`        
+${JSON.stringify(data, null, 4)}
+\`\`\`
+
+**[Edit](https://company-json.netlify.com/#!doc=${encodeURIComponent(JSON.stringify(data))})**`;
+}
+
+async function suggest(request, h) {
+    try {
+        const request_body = request.payload;
+
+        const result = await octokit.issues.create({
+            owner: config.suggest.owner,
+            repo: config.suggest.repo,
+            title: request_body.new
+                ? 'New company suggestion' + (request_body.data.name ? ': `' + request_body.data.name + '`' : '')
+                : 'Suggested update for company `' + request_body.data.slug + '`',
+            body: messageBody(request_body.data),
+        });
+
+        if (result.status !== 201) {
+            return h.response({ message: 'Failed to create GitHub issue.' }).code(502);
+        }
+
+        const issue_url = `https://github.com/${config.suggest.owner}/${config.suggest.repo}/issues/${result.data.number}`;
+
+        // We only know the issue URL to include in the sources *after* the issue has been created, unfortunately, so we need to edit it.
+        if (request_body.data.sources) request_body.data.sources.push(issue_url);
+        else request_body.data.sources = [issue_url];
+        await octokit.issues.update({
+            owner: config.suggest.owner,
+            repo: config.suggest.repo,
+            issue_number: result.data.number,
+            body: messageBody(request_body.data),
+        });
+
+        return h
+            .response({
+                message: 'Issue created successfully.',
+                issue_number: result.data.number,
+                issue_url: issue_url,
+            })
+            .code(201);
+    } catch (e) {
+        console.error(e);
+        return h.response({ message: 'Internal Server Error' }).code(500);
+    }
+}
+
+module.exports = suggest;
